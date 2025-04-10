a5c4b0fa99c59640336b3de78ba06b0a994ecf79
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-04-10 15:13:07 +0300

    add github auth

--- public/uiStuff.js
+++ public/uiStuff.js
@@ -24,12 +24,24 @@ document.querySelector(".name-form").addEventListener("submit", (e) => {
   document.querySelector(".player-name").innerHTML = player.name;
   loginModal.hide();
   spawnModal.show();
-  console.log(player);
 });
 
 document.querySelector(".start-game").addEventListener("click", (e) => {
   spawnModal.hide();
   const elArray = Array.from(document.querySelectorAll(".hiddenOnStart"));
   elArray.forEach((el) => el.removeAttribute("hidden"));
+  loginModal.hide();
   init();
 });
+
+document.querySelector(".btn-github").addEventListener("click", (e) => {
+  window.location.href = `http://localhost:9000/auth/github`;
+});
+
+const params = new URLSearchParams(window.location.search);
+const username = params.get("username");
+if (username) {
+  player.name = username;
+  loginModal.hide();
+  spawnModal.show();
+}
--- servers.js
+++ servers.js
@@ -2,10 +2,56 @@ import { Server } from "socket.io";
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+app.use(cors());
+
+const API_URL = process.env.API_URL;
+const CLIENT_ID = process.env.GITHUB_OAUTH_CLIENT_ID;
+const CLIENT_SECRET = process.env.GITHUB_OAUTH_CLIENT_SECRET;
+console.log(API_URL);
+
+app.get("/auth/github", (req, res) => {
+  const redirect_uri = `${API_URL}/auth/github/callback`;
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${redirect_uri}`
+  );
+});
+
+app.get("/auth/github/callback", async (req, res) => {
+  const { code } = req.query;
+
+  const tokenResponse = await axios.post(
+    `https://github.com/login/oauth/access_token`,
+    {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code,
+    },
+    {
+      headers: { Accept: "application/json" },
+    }
+  );
+
+  const access_token = tokenResponse.data.access_token;
+
+  const userResponse = await axios.get("https://api.github.com/user", {
+    headers: {
+      Authorization: `token ${access_token}`,
+    },
+  });
+
+  const username = userResponse.data.login;
+
+  res.redirect(`${API_URL}?username=${username}`);
+});
 
 app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(9000);
